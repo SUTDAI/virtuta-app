b77434389e3a265063568815f8458953d3c78b87
lohlifer <lohjaden25@gmail.com>
2024-01-31 02:05:20 +0800

    31 jan commit

---
+++ src/app/users/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const UsersPage = () => {
+  return (
+    <main>
+      <div>UsersPage</div>
+      <Link href="/">Back</Link>
+    </main>
+  );
+};
+
+export default UsersPage;
