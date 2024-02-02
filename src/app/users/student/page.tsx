40c883585f53257803c20e3b3cc7b86ed752dcec
Jaden Loh <lohjaden25@gmail.com>
2024-02-02 23:31:47 +0800

    Added student page

---
+++ src/app/users/student/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const StudentPage = () => {
+  return (
+    <main>
+      <div>Student</div>
+    </main>
+  );
+};
+
+export default StudentPage;
